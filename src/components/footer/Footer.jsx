b4a13d9f95c5109e5afff4db5e23e206ef1d8cb8
Rahul Kumar Saw <2021chb1052@iitrpr.ac.in>
2024-03-12 23:13:23 +0530

    some changes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -40,90 +40,17 @@ function Footer() {
               <li>
                 <Link to="/products">Products</Link>
               </li>
-              <li>
-                <Link to="https://github.com/chhakuli123">About Us</Link>
-              </li>
-              <li>
-                <Link to="https://www.linkedin.com/in/chhakuli-zingare-322986234">
-                  Contact Us
-                </Link>
-              </li>
-            </ul>
-          </div>
-          <div className="footer-section social">
-            <h2 className="footer-heading">Follow Us</h2>
-            <ul className="social-list">
-              <li>
-                <Link target="_blank" to="https://github.com/chhakuli123">
-                  <GitHubIcon className="nav-icon" />
-                </Link>
-              </li>
-              <li>
-                <Link target="_blank" to="https://twitter.com/ChhakuliZingare">
-                  <TwitterIcon className="nav-icon" />
-                </Link>
-              </li>
-              <li>
-                <Link
-                  target="_blank"
-                  to="https://www.instagram.com/chhakuli_zingare"
-                >
-                  <InstagramIcon className="nav-icon" />
-                </Link>
-              </li>
-              <li>
-                <Link
-                  target="_blank"
-                  to="https://www.linkedin.com/in/chhakuli-zingare-322986234"
-                >
-                  <LinkedInIcon className="nav-icon" />
-                </Link>
-              </li>
             </ul>
           </div>
         </div>
         <div className="desk-footer-bottom">
           <p>
-            &copy; 2023 Ecommerce Inc. All rights reserved By Chhakuli Zingare
+            &copy; 2023 Ecommerce All rights reserved 
           </p>
         </div>
       </footer>
       {/* ------------Mobile footer------------------- */}
       <footer className="mobile-footer">
-        <p className="footer-header">
-          Made with <span className="footer-logo">{`</>`}</span> by Chhakuli
-          Zingare
-        </p>
-        <div className="social-icons-container">
-          <Link
-            className="social-icons"
-            target="_blank"
-            to="https://github.com/chhakuli123"
-          >
-            <GitHubIcon className="social-icon" />
-          </Link>
-          <Link
-            className="social-icons"
-            target="_blank"
-            to="https://twitter.com/ChhakuliZingare"
-          >
-            <TwitterIcon className="social-icon" />
-          </Link>
-          <Link
-            className="social-icons"
-            target="_blank"
-            to="https://www.instagram.com/chhakuli_zingare"
-          >
-            <InstagramIcon className="social-icon" />
-          </Link>
-          <Link
-            className="social-icons"
-            target="_blank"
-            to="https://www.linkedin.com/in/chhakuli-zingare-322986234"
-          >
-            <LinkedInIcon className="social-icon" />
-          </Link>
-        </div>
         <p className="footer-bottom">© 2023 | ATTIREX</p>
       </footer>
     </>
